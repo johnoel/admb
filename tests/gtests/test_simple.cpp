9f2cd75a2df6bf99a433f67b9f3219df8354edde
johnoel <johnoel@admb-project.org>
2017-10-10 11:21:54 -1000

    Add test for comparing sequential with threaded.

--- tests/gtests/test_simple.cpp
+++ tests/gtests/test_simple.cpp
@@ -556,3 +556,122 @@ TEST_F(test_simple, sum_ax_b_threading)
   ASSERT_DOUBLE_EQ(grad0, sum(x));
   ASSERT_DOUBLE_EQ(expected, f);
 }
+void df_plus_eq_pvpv2(grad_stack_entry* grad_ptr)
+{
+  *grad_ptr->ind_addr1 += *grad_ptr->dep_addr;
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+}
+TEST_F(test_simple, sum_ax_b_threading_timer)
+{
+  ad_exit=&test_ad_exit;
+
+  //Increases gradient_structure::instances.
+  independent_variables independants(1, 2);
+  independants(1) = 5;
+  independants(2) = 10;
+
+  gradient_structure gs;
+  dvar_vector variables(independants);
+  
+  ASSERT_EQ(0, gradient_structure::GRAD_STACK1->ptr_index());
+  dvariable result = 0;
+  ASSERT_EQ(1, gradient_structure::GRAD_STACK1->ptr_index());
+
+  double expected = 0;
+  int index = 1; 
+  for (int i = 1; i <= 10; ++i)
+  {
+    ASSERT_EQ(index, gradient_structure::GRAD_STACK1->ptr_index());
+
+    //ax + b
+    result += variables(1) * x(i) + variables(2);
+    expected += independants(1) * x(i) + independants(2);
+
+    index += 3;
+    ASSERT_EQ(index, gradient_structure::GRAD_STACK1->ptr_index());
+  }
+
+  double f = value(result);
+  ASSERT_EQ(index, 31);
+  ASSERT_EQ(index, gradient_structure::GRAD_STACK1->ptr_index());
+
+  dvector g(1, 2);
+  g.initialize();
+
+  gradient_structure::GRAD_LIST->initialize();
+  ASSERT_DOUBLE_EQ(value(gradient_structure::RETURN_PTR[0]), 0.0);
+
+  double_and_int* tmp =
+    (double_and_int*)gradient_structure::get_ARRAY_MEMBLOCK_BASE();
+
+  unsigned long int imax = gradient_structure::ARR_LIST1->get_max_last_offset() / sizeof(double_and_int);
+  for (unsigned int i = 0; i < imax; ++i)
+  {
+    tmp->x = 0.0;
+    ++tmp;
+  }
+  for (int i = 30; i > 0; i -= 3)
+  {
+    grad_stack_entry* a = gradient_structure::GRAD_STACK1->get_element(i);
+    a->func2 = &df_plus_eq_pvpv2;
+  }
+  auto start = std::chrono::system_clock::now();
+  unsigned int arrayindex = 20;
+  std::vector<std::thread> threads;
+  for (int i = 30; i > 0; i -= 3)
+  {
+    threads.push_back(std::thread([&result](int i, int arrayindex, double xi)
+    {
+      grad_stack_entry* a = gradient_structure::GRAD_STACK1->get_element(i);
+      //DataRace with next line
+      *a->dep_addr = 1.0;
+      (*(a->func2))(a);
+
+      grad_stack_entry* b = gradient_structure::GRAD_STACK1->get_element(i - 1);
+      *b->dep_addr = 1.0;
+      (*(b->func2))(b);
+
+      grad_stack_entry* c = gradient_structure::GRAD_STACK1->get_element(i - 2);
+      *c->dep_addr = 1.0;
+      (*(c->func2))(c);
+    }, i, arrayindex, x(i / 3)));
+    arrayindex -= 2;
+  }
+  std::for_each(threads.begin(), threads.end(), [](std::thread &t) 
+  {
+    t.join();
+  });
+  double grad0 = 0.0;
+  double* grad_ptr0 = gradient_structure::get_INDVAR_LIST()->get_address(0);
+  double grad1 = 0.0;
+  double* grad_ptr1 = gradient_structure::get_INDVAR_LIST()->get_address(1);
+  for (int i = 30; i > 0; --i)
+  {
+    grad_stack_entry* gs = gradient_structure::GRAD_STACK1->get_element(i);
+    if (gs->ind_addr1 == grad_ptr0)
+    {
+      grad0 += gs->ind_value1;
+    }
+    if (gs->ind_addr2 == grad_ptr0)
+    {
+      grad0 += gs->ind_value2;
+    }
+    if (gs->ind_addr1 == grad_ptr1)
+    {
+      grad1 += gs->ind_value1;
+    }
+    if (gs->ind_addr2 == grad_ptr1)
+    {
+      grad1 += gs->ind_value2;
+    }
+  }
+  auto end = std::chrono::system_clock::now();
+  std::chrono::duration<double> elapsed_seconds = end - start;
+  auto start2 = std::chrono::system_clock::now();
+  gradcalc(2, g);
+  auto end2 = std::chrono::system_clock::now();
+  std::chrono::duration<double> elapsed_seconds2 = end2 - start2;
+  cout << "Time: " 
+       << elapsed_seconds.count() << ' ' << elapsed_seconds2.count() << endl;
+  ASSERT_TRUE(elapsed_seconds.count() < elapsed_seconds2.count());
+}
